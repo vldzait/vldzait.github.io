a6ac7dab8bb44457331526c329af39a9aeba0bfc
va_zaytsev <va_zaytsev@betboom.org>
2025-03-09 14:00:24 +0300

    feat: change info about me

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -7,9 +7,10 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Текст писать тут
-        </p>
+        <p>Название: Зайцев Вова</p>
+        <p>Cтек: React(Next) + Node(express) + MySql/Postrgres</p>
+        <p>Цель: структурировать и укрепить старые знания + подчерпнуть для себя что-то новое</p>
+        <p>О себе: тружусь программистом уже около 6 лет.</p>
       </header>
     </div>
   );
